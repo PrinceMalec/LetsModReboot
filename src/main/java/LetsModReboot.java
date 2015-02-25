3ed4dc2a8912b5fadd01979078c04c219b5a39c5
PrinceMalec <Kallax@hotmail.co.uk>
2015-02-25 20:39:13 +0000

    resources

---
+++ src/main/java/LetsModReboot.java
@@ -0,0 +1,51 @@
+import com.malec.letsmodreboot.handler.ConfigurationHandler;
+import com.malec.letsmodreboot.init.ModBlocks;
+import com.malec.letsmodreboot.init.ModItems;
+import com.malec.letsmodreboot.init.Recipes;
+import com.malec.letsmodreboot.proxy.IProxy;
+import com.malec.letsmodreboot.reference.Reference;
+import com.malec.letsmodreboot.utility.LogHelper;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Malec on 21/02/2015.
+ */
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
+
+public class LetsModReboot {
+    @Mod.Instance(Reference.MOD_ID)
+    public static LetsModReboot instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
+
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        FMLCommonHandler.instance().bus().register(new ConfigurationHandler());
+
+        ModItems.init();
+        ModBlocks.init();
+        LogHelper.debug("Pre Init Complete");
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        Recipes.init();
+        LogHelper.debug("Init Complete");
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        LogHelper.info("Post Init Complete");
+    }
+}
